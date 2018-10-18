5582f830d439a6039b03f8910e24cbca4626f426
Joe McBride <joseph.mcbride@extendhealth.com>
2018-10-17 17:23:26 -0700

    Add verbosity flag for tests

--- src/test.js
+++ src/test.js
@@ -1,6 +1,7 @@
 import exec from './exec'
 
 export default function test(settings) {
-  const cmd = `dotnet test "${settings.projectTestCsProjPath}" -c ${settings.target} --no-restore`
+  let verbosity = settings.testVerbosity || settings.CI ? 'normal' : 'minimal'
+  const cmd = `dotnet test "${settings.projectTestCsProjPath}" -c ${settings.target} --no-restore --verbosity ${verbosity}`
   return exec(cmd)
 }
